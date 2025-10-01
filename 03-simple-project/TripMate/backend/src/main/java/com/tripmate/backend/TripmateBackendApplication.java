e70c1cde958ac7c9b71a762197af87c8346d36ca
Lee997a <ehddn5476@gmail.com>
2025-10-01 17:45:03 +0900

    TripMate Prj

---
+++ 03-simple-project/TripMate/backend/src/main/java/com/tripmate/backend/TripmateBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tripmate.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripmateBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripmateBackendApplication.class, args);
+	}
+
+}
